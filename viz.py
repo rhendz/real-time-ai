57689f6871dc3f0dc5778043bbaa21e1ac8ee306
rhendz <apatel79@vols.utk.edu>
2019-07-05 11:47:06 -0400

    update

--- viz.py
+++
@@ -1,6 +0,0 @@
-from keras.utils import plot_model
-from keras.models import load_model
-
-model = load_model('saved_models/keras_cifar4_four_model.h5')
-
-plot_model(model, to_file='images/model.jpg', show_shapes=True)
